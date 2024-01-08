6fe9d64bab02b421e9dd570808c38a562aaf83c0
pratik-hanmante <pratikhanmante3@gmail.com>
2024-01-08 23:24:20 +0530

    created routers and helpers to authenticate user

--- src/controllers/authentication.ts
+++ src/controllers/authentication.ts
@@ -1,9 +1,71 @@
+import { createUser, getUsersByEmail } from "../db/users";
 import express from "express";
+import { random,authentication } from "../helpers";
 
-export const register =async (req:express.Request, res:express.Response) => {
+export const login = async(req: express.Request, res: express.Response) => {
     try {
-        const{email, password, username } = req.body;
-    }catch(error) {
+        const { email, password } = req.body;
+
+        if(!email || !password) {
+            return res.sendStatus(400);
+        }
+
+
+        const user = await getUsersByEmail(email).select('+authentication.salt +authentication.password');
+
+        if(!user) {
+            return res.sendStatus(400);
+        }
+
+        const expectedHash = authentication(user.authentication.salt, password);
+
+        if(user.authentication.password != expectedHash) {
+            res.sendStatus(403);
+        }
+
+        const salt = random();
+        user.authentication.sessionToken = authentication(salt, user._id.toString());
+
+        await user.save();
+
+        res.cookie("PRATIK-AUTH", user.authentication.sessionToken, {domain: "localhost", path: "/"})
+
+        return res.status(200).json(user).end();
+
+    } catch (error) {
+        console.log(error);
+        return res.sendStatus(400);
+    }
+}
+
+export const register = async (req:express.Request, res:express.Response) => {
+    try {
+        const {email, password, username } = req.body;
+
+        // check if anything is missing
+        if(!email || !password || !username ) {
+            return res.sendStatus(400);
+        }
+
+        const existingUser = await getUsersByEmail(email);
+
+        if(existingUser) {
+            return res.sendStatus(400);
+        }
+        
+        const salt = random();
+        const user = await createUser({
+            email,
+            username,
+            authentication: {
+                salt,
+                password: authentication(salt, password),
+            },
+
+        });
+
+        return res.status(200).json(user).end();
+    } catch(error) {
         console.log(error);
         return res.sendStatus(400);
     }
